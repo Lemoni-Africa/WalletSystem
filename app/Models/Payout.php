3510bcedc676de4d5e674a7d7fbfad4554223692
lemoniDev <support@lemoniafrica.com>
2022-07-24 20:04:46 +0100

    ss

--- app/Models/Payout.php
+++ app/Models/Payout.php
@@ -76,6 +76,8 @@ class Payout extends Model
         $this->narration = $data['data']['narration'];
         $this->provider = Providers::CRUST->value;
         $this->transactionStatus = TransactionStatus::FAILED->value;
+        $this->customerId = $request->customerId;
+        $this->reversal_url = $request->reversalUrl;
         $this->save();
 
         return $this;
